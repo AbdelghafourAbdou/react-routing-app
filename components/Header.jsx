2f26e6e5ce297a5ae080da0e17f11fcb4c76ea90
Abdelghafour Abdou <abdelghafourabdou@gmail.com>
2024-02-21 13:34:26 +0100

    changed styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Header = () => {
+    const headers = ['/host', '/about', '/vans'];
+    let bolds = [0, 0, 0];
+    const location = useLocation();
+    const wantedStyle = {textDecorationLine: 'underline',
+        fontWeight: '900'
+    };
+
+    const chosenPath = headers.filter((header) => (location.pathname.startsWith(header)));
+
+    headers.map((header, index) => {
+        chosenPath[0] === header ? bolds[index] = 1 : null;
+    });
+
+    function isSelected(index) {
+        return bolds[index] === 1 ? wantedStyle : {}
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <Link to="/host">Host</Link>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+                <Link style={isSelected(0)}  to="/host">Host</Link>
+                <Link style={isSelected(1)} to="/about">About</Link>
+                <Link style={isSelected(2)} to="/vans">Vans</Link>
             </nav>
         </header>
     )
