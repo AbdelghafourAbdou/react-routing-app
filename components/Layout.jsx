a5ced88c301a143a879b82194ea3c3b3caa041ff
Abdelghafour Abdou <abdelghafourabdou@gmail.com>
2024-02-21 11:33:41 +0100

    added footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,14 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from './Header'
-// import DashboardHeader from './DashboardHeader';
+import Footer from './Footer'
 
 const Layout = () => {
-  //const path = useLocation();
   
   return (
     <>
     <Header />
-    {/*path.pathname.startsWith('/host') ? <DashboardHeader /> : null*/}
     <Outlet />
+    <Footer />
     </>
   )
 }
